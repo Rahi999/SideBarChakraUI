1b26a8242d852f69f71f66283d146c2e5f1dbf4d
Rahim Ansari <rahimansari4560@gmail.com>
2022-05-27 17:25:04 +0530

    sidebar build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Styles from "./components/Styles";
+import ToastExample from "./components/ChakraUi"
+
+
+export default function App() {
+  return (
+    <div className="App">
+
+      
+       <ToastExample />
+    </div>
+  );
+}
